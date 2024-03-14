196bb1cb1fdc3c0fd99be4120ce020433dfdbdff
MladenStankov <mladen.stankov17@gmail.com>
2024-03-14 12:05:17 +0200

    database

--- backend/source/database/database.sql
+++ backend/source/database/database.sql
@@ -2,7 +2,10 @@ CREATE DATABASE IF NOT EXISTS fin_finder;
 
 CREATE TABLE IF NOT EXISTS fishes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    longitude DECIMAL(4, 10) NOT NULL,
-    attitude DECIMAL(4, 10) NOT NULL
+    scientificName VARCHAR(255) UNIQUE NOT NULL,
+    decimalLatitude DECIMAL(4, 10) NOT NULL,
+    decimalLongitude DECIMAL(4, 10) NOT NULL,
+    locality VARCHAR(255) NOT NULL,
+    depth INT,
+    depthAccuracy DECIMAL(2, 2) 
 );
