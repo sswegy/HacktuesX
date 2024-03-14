f134f215c5d1a928a0b1c646857616a00fe4ced8
Viktor Velkov <20viko06@gmail.com>
2024-03-15 00:27:53 +0200

    added buttona, not done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import React from "react";
 import GlobeMap from "./components/globeMap.js";
 import SearchBar from "./components/searchBar.js";
+import SideInfoButton from "./components/sideInfoButton.js";
 
 function App() {
   return (
     <div className="container">
-      <SearchBar />
+      <div className="searchbar-container">
+        <SearchBar />
+        <SideInfoButton />
+      </div>
       <GlobeMap />
     </div>
   );
